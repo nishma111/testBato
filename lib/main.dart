baad0f3826f016044c007763354b85472cb5a9ee
xroshan <roshanshrestha937@gmail.com>
2020-06-10 16:51:33 -0500

    firebase integration with 2 pinpoints on map

--- lib/main.dart
+++ lib/main.dart
@@ -8,25 +8,12 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Test Bato',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Test Bato'),
@@ -37,15 +24,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -53,16 +31,38 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  //Change Here
+  final String user = 'first';
   GoogleMapController mapController;
+  List<Marker> markers = [];
+  int index = 0;
+
+  @override
+  void initState() {
+    _pullData();
+    super.initState();
+  }
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
 
     mapController.animateCamera(
       CameraUpdate.newCameraPosition(
-        CameraPosition(target: LatLng(0, 0), zoom: 12.0),
+        CameraPosition(target: LatLng(0, 0), zoom: 15.0),
       ),
     );
+  }
+
+  void _pullData() async {
+    QuerySnapshot querySnapshot =
+        await Firestore.instance.collection("locations").getDocuments();
+    querySnapshot.documents.forEach((doc) {
+      markers.add(Marker(
+          markerId: MarkerId(doc.documentID),
+          position:
+              LatLng(doc['latitude'].toDouble(), doc['longitude'].toDouble()),
+          infoWindow: InfoWindow(title: doc.documentID)));
+    });
   }
 
   void _changeLatLng() async {
@@ -70,66 +70,87 @@ class _MyHomePageState extends State<MyHomePage> {
         .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
 
     // current position latitude and longitude
-    Map<String, dynamic> pos = {
+    Map<String, double> pos = {
       'latitude': position.latitude,
       'longitude': position.longitude
     };
 
-    print('\n\n\n');
-    print('Firestore data log..');
-
     // Update values of latitude and longitude in firebase
-    Firestore.instance
-        .collection('locations')
-        .document('2QtEq4ewkbHVrP19rRSx')
-        .updateData(pos);
-
-    // print latitude and longitude values stored in firebase
-    Firestore.instance
-        .collection('locations')
-        .document('2QtEq4ewkbHVrP19rRSx')
-        .get()
-        .then((DocumentSnapshot ds) {
-      print(ds.data);
-    });
-
-    print('\n\n\n');
+    Firestore.instance.collection('locations').document(user).updateData(pos);
 
     mapController.animateCamera(
       CameraUpdate.newCameraPosition(
         CameraPosition(
-            target: LatLng(position.latitude, position.longitude), zoom: 12.0),
+            target: LatLng(position.latitude, position.longitude), zoom: 15.0),
       ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-        appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: GoogleMap(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Stack(children: <Widget>[
+        GoogleMap(
             onMapCreated: _onMapCreated,
             initialCameraPosition: CameraPosition(
               target: LatLng(0, 0),
-              zoom: 12.0,
+              zoom: 15.0,
             ),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _changeLatLng,
-          tooltip: 'Increment',
-          child: Icon(Icons.pin_drop),
-        ));
+            markers: markers.toSet()),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(15.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  FloatingActionButton(
+                    backgroundColor: Color(0xff212f3d),
+                    child: Icon(Icons.add),
+                    onPressed: _changeLatLng,
+                  ),
+                  Expanded(child: Container()),
+                  FloatingActionButton(
+                    backgroundColor: Color(0xff212f3d),
+                    child: Icon(Icons.arrow_back_ios),
+                    onPressed: () {
+                      print(index);
+                      index = (index - 1) % markers.length;
+                      mapController.animateCamera(
+                        CameraUpdate.newCameraPosition(
+                          CameraPosition(
+                              target: markers[index].position, zoom: 15.0),
+                        ),
+                      );
+                    },
+                  ),
+                  SizedBox(
+                    width: 6,
+                  ),
+                  FloatingActionButton(
+                    backgroundColor: Color(0xff212f3d),
+                    child: Icon(Icons.arrow_forward_ios),
+                    onPressed: () {
+                      print(index);
+                      index = (index + 1) % markers.length;
+                      mapController.animateCamera(
+                        CameraUpdate.newCameraPosition(
+                          CameraPosition(
+                              target: markers[index].position, zoom: 15.0),
+                        ),
+                      );
+                    },
+                  )
+                ],
+              ),
+            ),
+          ],
+        )
+      ]),
+    );
   }
 }
